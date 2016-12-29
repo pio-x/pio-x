f1142b42843b6c868b2312c515088eae0fa4b666
Demian Holderegger <demian.holderegger@gmail.com>
2016-12-29 17:52:49 +0100

    show current user location on map

--- app/src/pages/map/map.ts
+++ app/src/pages/map/map.ts
@@ -17,10 +17,17 @@ export class MapPage {
     map: any;
 
     stations: Station[] = [];
-
     markers: any = [];
-
     infoWindow: any;
+    userLocation: any;
+    userMarker: any;
+
+    locationWatch: any;
+    locationWatchOptions: {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
 
     constructor(public navCtrl: NavController, private stationService: StationService) {
         this.updateStations();
@@ -39,14 +46,17 @@ export class MapPage {
                     this.addMarker(station)
                 })
             });
-
-
     }
 
     addMarker(station: Station) {
         let marker = new google.maps.Marker({
             map: this.map,
-            animation: google.maps.Animation.DROP,
+            icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                strokeColor: station.color,
+                strokeWeight: 3,
+                scale: 10
+            },
             position: new google.maps.LatLng(station.lat, station.long),
             station: station
         });
@@ -55,11 +65,11 @@ export class MapPage {
 
         let _this = this;
         google.maps.event.addListener(marker, 'click', function () {
-            _this.infoWindow.setContent(this.station.name);
+            let content = '<h2>'+this.station.name+'</h2>';
+            content += this.station.description;
+            _this.infoWindow.setContent(content);
             _this.infoWindow.open(_this.map, this);
         });
-
-
     }
 
     clearMarkers() {
@@ -68,26 +78,51 @@ export class MapPage {
         }
     }
 
-    addInfoWindow(content) {
-        this.infoWindow = new google.maps.InfoWindow({
-            content: content
+    updateUserMarker() {
+        if (this.userMarker != null) {
+            this.userMarker.setMap(null);
+        }
+        this.userMarker = new google.maps.Marker({
+            position: this.userLocation,
+            map: this.map,
+            icon: '/assets/bluecircle.png'
         });
     }
 
+    userLocationUpdated(position) {
+        console.log(position);
+        this.userLocation = new google.maps.LatLng({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        });
+        this.updateUserMarker();
+    }
 
     loadMap() {
 
         let latLng = new google.maps.LatLng(47.499002, 8.728729);
-
         let mapOptions = {
             center: latLng,
             zoom: 13,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-
         this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
-        this.addInfoWindow('Laden ...');
+        this.infoWindow = new google.maps.InfoWindow({
+            content: 'Laden ...'
+        });
+
+        // Try HTML5 geolocation
+        if (navigator.geolocation) {
+            this.locationWatch = navigator.geolocation.watchPosition(
+                (pos) => { this.userLocationUpdated(pos)} ,
+                (err) => { console.log('ERROR getting user location', err) },
+                this.locationWatchOptions
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            alert('Keine GPS Position gefunden!');
+        }
 
     }
 }
