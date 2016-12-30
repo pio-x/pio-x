a3d9bd74628312f4d63cf0da8de5a0430a76a668
Carlos Diener <carlos.diener@usystems.ch>
2016-12-30 21:04:26 +0100

    qr code library added

--- backend/js/app.js
+++ backend/js/app.js
@@ -1,4 +1,4 @@
-angular.module('backendApp', [])
+angular.module('backendApp', ['monospaced.qrcode'])
     .controller('QRCtrl', function($scope, $http){
         $http.get('https://api.pio-x.ch/index.php/team').then(function(articlesResponse) {
             $scope.groups = articlesResponse.data;
@@ -6,4 +6,5 @@ angular.module('backendApp', [])
         $http.get('https://api.pio-x.ch/index.php/riddle').then(function(articlesResponse) {
             $scope.riddles = articlesResponse.data;
         });
+        $scope.qrUrl = "https://app.pio-x.ch/login.html?team=1&hash=111";
 });
