ece1c6302197eafab1f56e5f336e3595baea3797
Demian Holderegger <demian.holderegger@gmail.com>
2016-12-30 11:57:52 +0100

    add login page and send login tokens to api

--- app/src/services/pioxApi.service.ts
+++ app/src/services/pioxApi.service.ts
@@ -1,15 +1,12 @@
-import { Console } from '@angular/compiler/src/private_import_core';
 import { Injectable }    from '@angular/core';
 import { Headers, Http, RequestOptions, Response } from '@angular/http';
 import {Observable} from 'rxjs/Rx';
+import {LocationService} from "./location.service";
 
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class PioxApiService {
-
-  private headers = new Headers({'Content-Type': 'application/json'});
-  private options = new RequestOptions({ headers: this.headers });
 
   private get baseurl() {
       if (window.location.hostname == 'app.pio-x.ch') {
@@ -21,17 +18,25 @@ export class PioxApiService {
       }
   }
 
-  constructor(private http: Http) {
+  constructor(private http: Http, private locationService: LocationService) {
+  }
+
+  private getOptions(): RequestOptions {
+    let headers = new Headers({'Content-Type': 'application/json'});
+    headers.append('X-Piox-Team', localStorage.getItem('team'));
+    headers.append('X-Piox-Hash', localStorage.getItem('hash'));
+    headers.append('X-Piox-Location', JSON.stringify(this.locationService.getLocation()));
+    return new RequestOptions({ headers: headers });
   }
 
   get(url): Promise<any> {
-    return this.http.get(this.baseurl + url)
+    return this.http.get(this.baseurl + url, this.getOptions())
                .toPromise()
                .then(response => response.json())
                .catch(this.handleError);
   }
   post(url, data): Promise<any> {
-    return this.http.post(this.baseurl + url, data, this.options)
+    return this.http.post(this.baseurl + url, data, this.getOptions())
                     .toPromise()
                     .then(response => response.json())
                     .catch(this.handleError);
