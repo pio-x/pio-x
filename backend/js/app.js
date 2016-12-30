9494d62ed521f4f9ff792397523c292563eb7fbb
Carlos Diener <carlos.diener@usystems.ch>
2016-12-30 16:29:49 +0100

    team listing from api on qr code tab

--- backend/js/app.js
+++ backend/js/app.js
@@ -1,1 +1,6 @@
-angular.module('backendApp', []);
+angular.module('backendApp', [])
+    .controller('QRCtrl', function($scope, $http){
+        $http.get('https://api.pio-x.ch/index.php/team').then(function(articlesResponse) {
+            $scope.qrCodes = articlesResponse.data;
+        });
+});
