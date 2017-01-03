c7fc6779321c92e19d59daa347cc09178314e954
Demian Holderegger <demian.holderegger@gmail.com>
2017-01-03 20:41:58 +0100

    add logout / cache flush option

--- app/src/pages/leaderboard/leaderboard.ts
+++ app/src/pages/leaderboard/leaderboard.ts
@@ -3,19 +3,21 @@ import { Team } from '../../interfaces/team';
 import { TeamService } from '../../services/team.service';
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-leaderboard',
   templateUrl: 'leaderboard.html'
 })
 export class LeaderboardPage {
-  teams:Team[] = [];
-  myteam:number = 0;
+  teams: Team[] = [];
+  myteam: number = 0;
+  myname: string = '';
 
-  constructor(public navCtrl: NavController, private teamService:TeamService) {
+  constructor(public navCtrl: NavController, private teamService:TeamService, public actionSheetCtrl: ActionSheetController) {
     this.updateLeaderboard();
     this.myteam = parseInt(localStorage.getItem('team'));
+    this.myname = decodeURIComponent(localStorage.getItem('player'));
   }
 
   sort(teams): Team[] {
@@ -26,6 +28,34 @@ export class LeaderboardPage {
   updateLeaderboard(): void {
     this.teamService.getTeams()
       .then(teams => this.teams = this.sort(teams));
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+        title: 'Optionen',
+        buttons: [
+            {
+                text: 'Cache löschen',
+                handler: () => {
+                    // according to stackoverflow this should force a reload from the server
+                    window.location.reload(true);
+                }
+            },
+            {
+                text: 'Abmelden',
+                handler: () => {
+                    window.location.href = 'login.html?logout=true';
+                }
+            },
+            {
+                text: 'Abbrechen',
+                role: 'cancel',
+                handler: () => {}
+            }
+        ]
+    });
+
+    actionSheet.present();
   }
 
 }
