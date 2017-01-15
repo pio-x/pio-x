57e4d7e5b304399c86fcc4a320773aa245b23b29
Demian Holderegger <demian.holderegger@gmail.com>
2017-01-15 16:30:40 +0100

    enable prod mode on other hosts than localhost

--- app/src/app/main.ts
+++ app/src/app/main.ts
@@ -1,5 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+import {enableProdMode} from "@angular/core";
+
+// dev mode only on localhost
+if (window.location.hostname != 'localhost') {
+    enableProdMode()
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
