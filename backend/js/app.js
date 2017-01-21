05d1f5eac9e9c7cfa3030b49021b662035e71fce
Carlos Diener <carlos.diener@usystems.ch>
2017-01-21 17:29:39 +0100

    backend: img > stream tab

--- backend/js/app.js
+++ backend/js/app.js
@@ -102,6 +102,27 @@ var backendApp = angular.module('backendApp', ['monospaced.qrcode'])
                 });
         };
     })
+    //LOG
+    .controller('logCtrl', function($scope, apiService){
+        $scope.getLogs = function() {
+            apiService.get('/log').then(function(articlesResponse) {
+                $scope.logs = articlesResponse.data;
+            });
+        };
+        $scope.getLogs();
+        $scope.updateLog = function(id, data) {
+            apiService.put('/log/' + id, data)
+                .then(function(){
+                    $scope.getLogs();
+                });
+        };
+        $scope.deleteLog = function(id) {
+            apiService.delete('/log/' + id)
+                .then(function(){
+                    $scope.getLogs();
+                });
+        };
+    })
     //PASSCODES
     .controller('passcodeCtrl', function($scope, apiService){
         $scope.getCodes = function() {
