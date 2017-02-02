64a99e9511914517ff2c35d29535cead3810fa52
Carlos Diener <carlos.diener@usystems.ch>
2017-02-02 19:33:59 +0100

    moved map to a directive

---
+++ backend/js/directives/ngBackendMap.js
@@ -0,0 +1,6 @@
+backendApp.directive('backendMap', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'js/view/backend-map.html'
+  };
+});
---
+++ backend/js/directives/ngMapInfo.js
@@ -0,0 +1,6 @@
+backendApp.directive('mapInfo', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'js/view/map-info.html'
+  };
+});
