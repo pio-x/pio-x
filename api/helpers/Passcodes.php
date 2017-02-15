036cd2fe3507dc6033ab30f8f8b036c76d744594
Laurenz Gamper <laurenzgamper@gmail.com>
2017-02-15 20:58:12 +0100

    Passcodes aus normalen Wörtern erstellen

---
+++ api/helpers/Passcodes.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once("Passcodestrings.php");
+
+function randomWord($pwWords, $min, $max) {
+    $word = $pwWords[floor(rand(0, count($pwWords)-1))];
+    while (count($word) > $min and count($word) < $max ) {
+        $word = $pwWords[floor(rand(0, count($pwWords)-1))];
+    }
+    return $word;
+}
+
+function passcode($words) {
+    global $pwWords;
+    $passcode = '';
+
+    for ($i=0;$i<$words;$i++) {
+        $passcode .= randomWord($pwWords, 3,7);
+    }
+    
+    return $passcode;
+}
