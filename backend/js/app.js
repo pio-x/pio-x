616b6eff18de5a9404e61e79b835e8fe499cd35f
Carlos Diener <carlos.diener@usystems.ch>
2017-02-19 16:36:23 +0100

    newriddle form on map

--- backend/js/app.js
+++ backend/js/app.js
@@ -11,23 +11,26 @@ var backendApp = angular.module('backendApp', ['monospaced.qrcode', 'ngMap'])
         $scope.showTeams = true;
         $scope.showTeam = true;
         $scope.showTeamLocation = false;
-        $scope.showNewStation = true;
+        $scope.showNewObjectInput = 'station';
         $scope.showNewRiddle = false;
-        
+
         $scope.createNewStation = function() {
-            
             var data = {
                 name: $scope.newStationName,
-                description: $scope.newStationDescription,
+                description: $scope.newStation.description,
                 pos_lat: $scope.newlat,
                 pos_long: $scope.newlng
             };
             console.log(data);
-            apiService.post('/station', data)
+            console.log($scope.newStation.name);
+            console.log($scope.newStation.description);
+            console.log($scope.newlat);
+            console.log($scope.newlng);
+            /*apiService.post('/station', data)
                 .then(function(){
-                    $Scope.refreshData();
+                    $scope.refreshData();
                 }
-            );
+            );*/
         };
 
         $scope.showTooltip = function(evt, id, obj) {
