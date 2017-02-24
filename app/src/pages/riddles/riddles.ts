e977a8d57143b8b6bca456b024f56ca857ee1a69
Demian Holderegger <demian.holderegger@gmail.com>
2017-02-24 22:31:38 +0100

    add passcode button to riddle page

--- app/src/pages/riddles/riddles.ts
+++ app/src/pages/riddles/riddles.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Riddle } from "../../interfaces/riddle";
 import { RiddleService } from "../../services/riddle.service";
 import {NavigationService} from "../../services/navigation.service";
+import {PasscodePage} from "../passcode/passcode";
+import {ModalController} from "ionic-angular";
 
 @Component({
   selector: 'page-riddles',
@@ -14,7 +16,8 @@ export class RiddlesPage {
 
     constructor(
         private riddleService: RiddleService,
-        public navService: NavigationService
+        public navService: NavigationService,
+        public modalCtrl: ModalController
     ) {
 
         this.updateRiddles();
@@ -28,6 +31,11 @@ export class RiddlesPage {
     }
     presentActionSheet() {
         this.navService.presentActionSheet()
+    }
+
+    openPasscodeModal() {
+        let passcodeModal = this.modalCtrl.create(PasscodePage);
+        passcodeModal.present();
     }
 
     doRefresh(refresher) {
