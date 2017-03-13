1ca396b2180a40326615e14bcf5c37ce1ca2ff21
Demian Holderegger <demian.holderegger@gmail.com>
2017-03-13 11:29:20 +0100

    use angular-moment instead of plain moments library

--- backend/js/app.js
+++ backend/js/app.js
@@ -1,6 +1,11 @@
-var backendApp = angular.module('backendApp', ['monospaced.qrcode', 'ngMap', 'highcharts-ng'])
-    .filter('moment', function() {
-        return function(dateString, format) {
-            return moment(dateString).format(format);
-        };
-    });
+var backendApp = angular.module('backendApp', ['monospaced.qrcode', 'ngMap', 'highcharts-ng', 'angularMoment']);
+
+backendApp.filter('moment', function() {
+    return function(dateString, format) {
+        return moment(dateString).format(format);
+    };
+});
+
+backendApp.run(function(amMoment) {
+    amMoment.changeLocale('de');
+});
