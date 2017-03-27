f413f496c9d02145c32d408e42ac250a31230f5f
Demian Holderegger <demian.holderegger@gmail.com>
2017-03-27 16:28:35 +0200

    JS Fehlermeldungen optimieren und anzeigen closes #107

--- app/src/app/PioxErrorHandler.ts
+++ app/src/app/PioxErrorHandler.ts
@@ -4,10 +4,11 @@ export class PioxErrorHandler extends IonicErrorHandler {
 
   handleError(error) {
 
-    // show error to user on live app
-    // TODO: also show error in app
-    if (window.location.hostname == 'app.pio-x.ch') {
-      alert(error);
+    // show error to user on live app (do not show in dev mode)
+    if (window.location.port != '8100') {
+      //alert(error);
+      document.getElementById('js-error').style.display = 'block';
+      document.getElementById('js-error-text').innerHTML = 'Ein Fehler ist aufgetreten: ' + error;
     }
 
     super.handleError(error);
