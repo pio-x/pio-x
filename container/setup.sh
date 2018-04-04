8e996de0de7063bd0c93eda70af2d867e19d85cf
Laurenz Gamper <laurenzgamper@gmail.com>
2018-04-04 18:17:26 +0200

    fix ports and setup

--- container/setup.sh
+++ container/setup.sh
@@ -3,7 +3,7 @@
 #if [ ! -f /www/setup_done ]; then
 #    exit 0;
 #fi
-
+sleep 10;
 mysql -h mysql -u root --password=mysecretpioXdbpassword -e 'create database piox'
 mysql -h mysql -u root --password=mysecretpioXdbpassword piox < /www/dbdump.sql
 
