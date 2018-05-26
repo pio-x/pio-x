9b7b620b955cbaca54cb4f9cde180cf0c8997a6a
Demian Holderegger <demian.holderegger@gmail.com>
2018-05-26 23:24:48 +0200

    add more vars

--- api/dbdumps/v3/update3.sql
+++ api/dbdumps/v3/update3.sql
@@ -1,4 +1,6 @@
 INSERT INTO `config` (`key`, `value`) VALUES ('map_center_lat', '47.497422');
 INSERT INTO `config` (`key`, `value`) VALUES ('map_center_long', '8.722104');
 INSERT INTO `config` (`key`, `value`) VALUES ('passcode_page_text', 'Spielinfos auf der Passcode Seite');
+INSERT INTO `config` (`key`, `value`) VALUES ('passcode_page_title', 'Pio – X 2018 (Winterthur)');
+INSERT INTO `config` (`key`, `value`) VALUES ('passcode_page_subtitle', 'Der etwas andere Pio Wettkampf!');
 
