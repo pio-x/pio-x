cc50520107a215aef53e0ff34a79ee2562723281
Demian Holderegger <demian.holderegger@gmail.com>
2018-05-27 16:20:24 +0200

    fix diashow url

--- backend/js/controllers/diashowCtrl.js
+++ backend/js/controllers/diashowCtrl.js
@@ -1,5 +1,7 @@
 backendApp.controller('diashowCtrl', function($scope, $interval, apiService, teamService, logService, configService, stationService, mrxService){
 
+    $scope.apiURL = localStorage.getItem('api');
+
     $scope.diashowIndex = 0;
     $scope.diashowData = [];
     $scope.caption = '';
@@ -19,13 +21,14 @@ backendApp.controller('diashowCtrl', function($scope, $interval, apiService, tea
 
     $scope.refresh = function() {
         if ($scope.diashowData[$scope.diashowIndex + 1]) {
+            var baseUrl = $scope.apiURL + '/uploaded_images/';
             $scope.diashowIndex++;
-            $scope.backgroundStyle['background-image'] = 'url("../api/image/' + $scope.diashowData[$scope.diashowIndex].img_ID + '.jpg?hash=' + $scope.hash + '")';
+            $scope.backgroundStyle['background-image'] = 'url("' + baseUrl + $scope.diashowData[$scope.diashowIndex].img_ID + '.jpg?hash=' + $scope.hash + '")';
             $scope.caption = $scope.diashowData[$scope.diashowIndex].text;
 
             // cache next image
             if ($scope.diashowData[$scope.diashowIndex + 1]) {
-                preloadImage('../api/image/' + $scope.diashowData[$scope.diashowIndex + 1].img_ID + '.jpg?hash=' + $scope.hash);
+                preloadImage(baseUrl + $scope.diashowData[$scope.diashowIndex + 1].img_ID + '.jpg?hash=' + $scope.hash);
             }
         }
     };
