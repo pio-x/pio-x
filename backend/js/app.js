2d31fb0096a8c03d941ef62f49365b07eaac088b
Demian Holderegger <demian.holderegger@gmail.com>
2018-06-14 19:04:05 +0200

    install angular-sanitize

--- backend/js/app.js
+++ backend/js/app.js
@@ -1,10 +1,16 @@
-var backendApp = angular.module('backendApp', ['monospaced.qrcode', 'ngMap', 'highcharts-ng', 'angularMoment', 'ui.bootstrap']);
+var backendApp = angular.module('backendApp', ['monospaced.qrcode', 'ngMap', 'highcharts-ng', 'angularMoment', 'ui.bootstrap', 'ngSanitize']);
 
 backendApp.filter('moment', function() {
     return function(dateString, format) {
         return moment(dateString).format(format);
     };
 });
+
+backendApp.filter('to_trusted', ['$sce', function($sce){
+    return function(text) {
+        return $sce.trustAsHtml(text);
+    };
+}]);
 
 backendApp.run(function(amMoment) {
     amMoment.changeLocale('de');
