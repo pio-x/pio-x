4b34ccbcd54250a61158a38ca59321f0dba87b51
Demian Holderegger <demian.holderegger@gmail.com>
2018-11-06 01:18:26 -0200

    make Admin GUI more failsafe and consistent

--- backend/js/controllers/teamPointModalCtrl.js
+++ backend/js/controllers/teamPointModalCtrl.js
@@ -1,17 +1,18 @@
 var TeamPointModalCtrl = function ($scope, $uibModalInstance, $http, apiService, teamService, team_ID, team_name) {
-    $scope.points = 0;
-    $scope.team_ID = team_ID;
-    $scope.team_name = team_name;
+	$scope.points = 0;
+	$scope.team_ID = team_ID;
+	$scope.team_name = team_name;
 
 	$scope.cancel = function () {
 		$uibModalInstance.dismiss('cancel');
 	};
 
-    $scope.addTeamPoints = function(points) {
-        data = {"points": points};
-        apiService.post('/points/' + parseInt($scope.team_ID), data)
-            .then(function(){
-                teamService.update();
-            });
-    }
+	$scope.addTeamPoints = function(points) {
+		data = {"points": points};
+		apiService.post('/points/' + parseInt($scope.team_ID), data)
+			.then(function(){
+				teamService.update();
+				$scope.cancel();
+			});
+	}
 };
