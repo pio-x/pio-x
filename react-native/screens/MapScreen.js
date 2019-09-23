d60f2c4a1631f7a8a0fa504c0987c79151c0f940
Demian Holderegger <demian.holderegger@gmail.com>
2019-09-23 22:10:50 +0200

    add first map implementation

--- react-native/screens/MapScreen.js
+++ react-native/screens/MapScreen.js
@@ -5,15 +5,118 @@ import {
   View,
 } from 'react-native';
 import styled from 'styled-components'
+import MapView, { Marker, Callout } from 'react-native-maps';
+import {connect} from "react-redux";
 
-export default function MapScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <Text>Map</Text>;
+function distinctColor(id) {
+  if (id === null) {
+    // empty station
+    return '#bbbbbb';
+  }
+  // 32 distinct colors
+  let colors = ['#f23e22', '#d1c51d', '#77baad', '#1d1dd1', '#eb2172', '#f0a599', '#b0ac71', '#24fff0', '#9999f0', '#e391a7', '#c96f4f', '#bbeb5b', '#1b9ebf', '#7654d6', '#f2223e', '#ed7321', '#94c981', '#9ee6f7', '#9122f2', '#c74e5e', '#ebb896', '#1ed41e', '#23b2fa', '#a672b3', '#b28046', '#23f794', '#1d65d1', '#f563ff', '#f0ab22', '#18ad7c', '#a3c8ff', '#e359b5']
+  return colors[id % 32];
+}
+
+const CalloutView = styled.View`
+  width: 300px;
+`;
+const CalloutTitle = styled.Text`
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+const StationMarkerView = styled.View`
+  width: 20px;
+  height: 20px;
+  background-color: ${props => props.color + '55'};
+  border: 1px solid ${props => props.color};
+  border-radius: 20px;
+`;
+
+class StationMarker extends React.Component {
+  render() {
+
+    return <Marker
+      coordinate={{
+        latitude: this.props.station.pos_lat,
+        longitude: this.props.station.pos_long,
+      }}
+      title={this.props.station.name}
+      description={this.props.station.description}
+      key={this.props.station.s_ID}
+    >
+      <StationMarkerView
+        color={distinctColor(this.props.station.team)}
+      />
+      <Callout>
+        <CalloutView>
+          <CalloutTitle>{this.props.station.name}</CalloutTitle>
+          {this.props.station.team !== null
+              ? <Text>Diese Station gehört Team {this.props.station.team}</Text>
+              : <Text>Diese Station gehört keinem Team</Text>
+          }
+        </CalloutView>
+      </Callout>
+    </Marker>
+  }
+}
+
+class MapScreen extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={
+      isLoading: true,
+      stations: [],
+    }
+    this.loadData();
+  }
+
+  loadData() {
+    return fetch(this.props.auth.api_url + '/station?hash=' + this.props.auth.hash)
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        console.log(responseJson)
+        this.setState({
+          isLoading: false,
+          stations: responseJson,
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+  render() {
+    return <MapView style={{flex: 1}}
+                    initialRegion={{
+                      latitude: 47.4974253,
+                      longitude: 8.72199282,
+                      latitudeDelta: 0.0922,
+                      longitudeDelta: 0.0421,
+                    }}
+    >
+      {this.state.stations.map(station => (
+        <StationMarker
+          station={station}
+          key={station.s_ID}
+        />
+      ))}
+    </MapView>;
+  }
 }
 
 MapScreen.navigationOptions = {
   title: 'Karte',
 };
+
+const mapStateToProps = function(state) {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps)(MapScreen);
