9fcf0bc5ff40c0ed29d9e48f82ac1cc7f0b1a78d
Demian Holderegger <demian.holderegger@gmail.com>
2019-10-06 23:01:09 +0200

    show devmode state

--- react-native/screens/RiddlesScreen.js
+++ react-native/screens/RiddlesScreen.js
@@ -11,7 +11,10 @@ export default function RiddlesScreen() {
    * Go ahead and delete ExpoConfigView and replace it with your content;
    * we just wanted to give you a quick view of your config.
    */
-  return <Text>Rästel</Text>;
+  return <View>
+    <Text>Rätsel</Text>
+    <Text>Dev Mode: {__DEV__ ? 'ON' : 'OFF'}</Text>
+  </View>;
 }
 
 RiddlesScreen.navigationOptions = {
