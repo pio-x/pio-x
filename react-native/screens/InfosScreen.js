17e1df8cb37df2345600905f2d810157f7c4c585
Demian Holderegger <demian.holderegger@gmail.com>
2019-10-20 17:03:10 +0200

    trying webview, not working yet :/

--- react-native/screens/InfosScreen.js
+++ react-native/screens/InfosScreen.js
@@ -5,14 +5,19 @@ import {
   View,
   WebView
 } from 'react-native';
+//import { WebView } from 'react-native-webview';
+
 import styled from 'styled-components'
 
 export default function InfosScreen() {
-  return <WebView
-    style={{ flex: 1 }}
-    originWhitelist={['*']}
-    source={{html: '<h1>Hello world</h1><br><br>hallooo'}}
-  />
+  return <View style={{ flex: 1 }}>
+    <Text>Infos</Text>
+    <WebView
+      style={{ flex: 1, marginTop: 20 }}
+      originWhitelist={['*']}
+      source={{html: '<h1>Hello world</h1><br><br>not sure why, but this webview does not work'}}
+    />
+  </View>
 }
 
 InfosScreen.navigationOptions = {
