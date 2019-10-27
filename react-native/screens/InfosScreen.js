4ba6be43e771adf37313e753088330f340d23476
Demian Holderegger <demian.holderegger@gmail.com>
2019-10-27 16:20:38 +0100

    fix html renderer on infoscreen

--- react-native/screens/InfosScreen.js
+++ react-native/screens/InfosScreen.js
@@ -1,25 +1,91 @@
 import React from 'react';
 import {
-  StyleSheet,
   Text,
   View,
-  WebView
 } from 'react-native';
-//import { WebView } from 'react-native-webview';
+import { connect } from 'react-redux';
+import { WebView } from 'react-native-webview';
 
 import styled from 'styled-components'
 
-export default function InfosScreen() {
-  return <View style={{ flex: 1 }}>
-    <Text>Infos</Text>
-    <WebView
-      style={{ flex: 1, marginTop: 20 }}
-      originWhitelist={['*']}
-      source={{html: '<h1>Hello world</h1><br><br>not sure why, but this webview does not work'}}
-    />
-  </View>
+class InfosScreen extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={
+      htmlInfos: 'Infos werden geladen...'
+    };
+  }
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Infos'
+    };
+  };
+
+  wrapHtml(content) {
+    let page = '';
+    page += `<!doctype html>
+              <html lang="en">
+                <head>
+                  <meta charset="utf-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                  <style>
+                  body {
+                    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+                  }
+                  </style>
+                </head>
+                <body>
+                ${content}
+                </body>
+              </html>`;
+    return page;
+  }
+
+  loadData() {
+    this.setState({
+      htmlInfos: 'Infos werden geladen...'
+    });
+    return fetch(this.props.auth.api_url + '/config?hash=' + this.props.auth.hash)
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        if (responseJson && responseJson.info_text) {
+          this.setState({
+            htmlInfos: responseJson.info_text
+          });
+        } else {
+          this.setState({
+            htmlInfos: 'Infos konnten nicht geladen werden :('
+          });
+        }
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+  componentDidMount(){
+    this.loadData();
+  }
+
+  render() {
+    return <View style={{flex: 1}}>
+      <WebView
+          style={{flex: 1}}
+          originWhitelist={['*']}
+          source={{html: this.wrapHtml(this.state.htmlInfos)}}
+      />
+    </View>
+  }
 }
 
-InfosScreen.navigationOptions = {
-  title: 'Infos',
+const mapStateToProps = function(state) {
+  return {
+    auth: state.auth
+  }
 };
+
+export default connect(mapStateToProps)(InfosScreen);
